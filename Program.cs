51b446791bb1f164cf1ffd6b1bdd2dcc569f9a56
Trevor Earl Matheson <trevor.earl.matheson@gmail.com>
2020-12-22 15:01:04 -0700

    Added user response and factorial computation.

--- Program.cs
+++ Program.cs
@@ -6,13 +6,17 @@ namespace Factorial
     {
         static int Factorial(int number)
         {
-            
+            if(number > 0)
+            {
+                number *= Factorial(--number);
+            }
+            return(number);
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Enter a number you want to know the factorial of.");
             var entry = Console.ReadLine();
-
+            Console.WriteLine("{0}! = {1}", entry, Factorial(int.Parse(entry)));
         }
     }
 }
